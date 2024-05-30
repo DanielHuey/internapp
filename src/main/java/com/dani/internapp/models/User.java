923bea0a106fa5cdbf2da1baa965f1fdf2fad0ca
DanielHuey <adhueywilan@gmail.com>
2024-05-30 12:01:56 +0300

    User model

---
+++ src/main/java/com/dani/internapp/models/User.java
@@ -0,0 +1,38 @@
+package com.dani.internapp.models;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String dob;
+
+    public User(String firstname, String lastname, String username, String dob)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.dob = dob;
+    }
+
+    public boolean checkUsername(String s) {return this.username == s;}
+
+    /* getters */
+    public String getUsername() {return this.username;}
+
+    public String getName() {return this.firstname + " " + this.lastname;}
+
+    public String getFirstName() {return this.firstname;}
+
+    public String getLastName() {return this.lastname;}
+
+    public String getDOB() {return this.dob;}
+
+    /* setters */
+    public void setUsername(String s) {this.username = s;}
+
+    public void setFirstName(String s) {this.firstname = s;}
+
+    public void setLastName(String s) {this.lastname = s;}
+
+    public void setDOB(String s) {this.dob = s;}
+}
