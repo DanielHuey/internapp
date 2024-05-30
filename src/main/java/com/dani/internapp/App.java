e32aae42585899c200e6d748997d35b991f59bbf
DanielHuey <adhueywilan@gmail.com>
2024-05-30 12:07:29 +0300

    Entry Point & Test(autogenerated)

---
+++ src/main/java/com/dani/internapp/App.java
@@ -0,0 +1,27 @@
+package com.dani.internapp;
+
+import java.util.*;
+
+import com.dani.internapp.models.User;
+
+/**
+ * App class 001
+ *
+ */
+@SuppressWarnings("unused")
+public class App 
+{
+    public static void main( String[] args )
+    {
+        welcomeMessage();
+        System.out.println("\t1: Register User\n\t2: Retrieve All Users\n\t3: Retrieve by username\n\t4: Delete a user\n\t5: Exit");
+    }
+
+    static void welcomeMessage()
+    {
+        System.out.println("Welcome to the Registration System.");
+        System.out.println("Provide a number to make a choice:");
+    }
+
+    
+}
