5b029fecdb7035f9947ba26879102a3d47ec1337
DanielHuey <adhueywilan@gmail.com>
2024-05-31 16:50:35 +0300

    teamwork refactoring (dob->dateOfBirth)

--- src/main/java/com/dani/internapp/models/User.java
+++ src/main/java/com/dani/internapp/models/User.java
@@ -4,21 +4,21 @@ public class User {
     private String firstname;
     private String lastname;
     private String username;
-    private String dob;
+    private String dateOfBirth;
 
-    public User(String firstname, String lastname, String username, String dob)
+    public User(String firstname, String lastname, String username, String dateOfBirth)
     {
         this.firstname = firstname;
         this.lastname = lastname;
         this.username = username;
-        this.dob = dob;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public boolean checkUsername(String s) {return this.username == s;}
 
     @Override
     public String toString() {
-        return "\n User: " + this.username + "\n Full Name: " + this.getName() + "\n DOB: " + this.dob + "\n";
+        return "\n User: " + this.username + "\n Full Name: " + this.getName() + "\n Date Of Birth: " + this.dateOfBirth + "\n";
     }
 
     /* getters */
@@ -30,7 +30,7 @@ public class User {
 
     public String getLastName() {return this.lastname;}
 
-    public String getDOB() {return this.dob;}
+    public String getDateOfBirth() {return this.dateOfBirth;}
 
     /* setters */
     public void setUsername(String s) {this.username = s;}
@@ -39,5 +39,5 @@ public class User {
 
     public void setLastName(String s) {this.lastname = s;}
 
-    public void setDOB(String s) {this.dob = s;}
+    public void setDateOfBirth(String s) {this.dateOfBirth = s;}
 }
