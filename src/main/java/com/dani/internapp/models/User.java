e767943702b930c7793c3d36d93a401f5f3ce5e4
DanielHuey <adhueywilan@gmail.com>
2024-06-04 23:18:50 +0300

    Updates and Refactorings.

--- src/main/java/com/dani/internapp/models/User.java
+++ src/main/java/com/dani/internapp/models/User.java
@@ -1,43 +1,61 @@
 package com.dani.internapp.models;
+import java.util.Date;
+import java.util.Objects;
 
 public class User {
-    private String firstname;
-    private String lastname;
-    private String username;
-    private String dateOfBirth;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private Date dateOfBirth;
 
-    public User(String firstname, String lastname, String username, String dateOfBirth)
+    public User(String firstName, String lastName, String userName, Date dateOfBirth)
     {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
         this.dateOfBirth = dateOfBirth;
     }
 
-    public boolean checkUsername(String s) {return this.username == s;}
+    public boolean checkUserName(String s) {return this.userName.equals(s);}
 
     @Override
     public String toString() {
-        return "\n User: " + this.username + "\n Full Name: " + this.getName() + "\n Date Of Birth: " + this.dateOfBirth + "\n";
+        return "\n User: " + this.userName + "\n Full Name: " + this.getName() + "\n Date Of Birth: " + this.dateOfBirth + "\n";
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName,lastName,userName,dateOfBirth);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User other = (User) o;
+        return (Objects.equals(firstName, other.firstName) &&
+                Objects.equals(lastName, other.lastName) &&
+                Objects.equals(userName, other.userName) &&
+                Objects.equals(dateOfBirth, other.dateOfBirth));
     }
 
     /* getters */
-    public String getUsername() {return this.username;}
+    public String getUserName() {return this.userName;}
 
-    public String getName() {return this.firstname + " " + this.lastname;}
+    public String getName() {return this.firstName + " " + this.lastName;}
 
-    public String getFirstName() {return this.firstname;}
+    public String getFirstName() {return this.firstName;}
 
-    public String getLastName() {return this.lastname;}
+    public String getLastName() {return this.lastName;}
 
-    public String getDateOfBirth() {return this.dateOfBirth;}
+    public Date getDateOfBirth() {return this.dateOfBirth;}
 
     /* setters */
-    public void setUsername(String s) {this.username = s;}
+    public void setUserName(String s) {this.userName = s;}
 
-    public void setFirstName(String s) {this.firstname = s;}
+    public void setFirstName(String s) {this.firstName = s;}
 
-    public void setLastName(String s) {this.lastname = s;}
+    public void setLastName(String s) {this.lastName = s;}
 
-    public void setDateOfBirth(String s) {this.dateOfBirth = s;}
+    public void setDateOfBirth(Date s) {this.dateOfBirth = s;}
 }
